7bae92c9ffee08577642439425407d49e1c90886
Sefa Ertunç <sefaertnc@gmail.com>
2024-01-09 19:13:54 +0300

    UN: Tidy Up

--- Coffee Machine Sim/CoffeeMachine.py
+++ Coffee Machine Sim/CoffeeMachine.py
@@ -1,49 +1,19 @@
-from CoffeeMach_Data import MENU
-from CoffeeMach_Data import RESOURCES
 from prettytable import PrettyTable
 
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-money = 0
 is_process_over = False
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
 
-
-def identify_inserted_coins(q, d, n, p):
-    print("Please insert coins.")
-    quarter = int(input("How many quarters?: "))
-    dime = int(input("How many dimes?: "))
-    nick = int(input("How many nickles?: "))
-    penn = int(input("How many pennies?: "))
-    return quarter * q + dime * d + nick * n + penn * p
-
-
-def activate_operator(inserted_coins, userinput):
-    global money
-    cost = MENU[userinput]["cost"]
-    if inserted_coins >= cost:
-        earned = cost
-        exchange = inserted_coins - earned
-        money += earned
-        if exchange > 0:
-            print(f"Here is ${exchange:.2f} in change.")
-        make_coffee(userinput)
-    else:
-        print("Sorry that's not enough money. Money refunded.")
-
-
-def check_sources(userinput):
-    for ingredient in MENU[userinput]["ingredients"]:
-        if MENU[userinput]["ingredients"][ingredient] > RESOURCES[ingredient]:
-            return False
-    return True
-
-
-def make_coffee(userinput):
-    for ingredient in MENU[userinput]["ingredients"]:
-        RESOURCES[ingredient] -= MENU[userinput]["ingredients"][ingredient]
-    print(f"Here is your {userinput} ☕ Enjoy!")
+def make_table():
+    table = PrettyTable()
+    table.add_column("Coffee Types", ["Espresso", "Latte", "Cappuccino"])
+    table.add_column("Price", ["1.50$", "2.50$", "3.00$"])
+    print(table)
 
 
 def request_check(user_input):
@@ -62,14 +32,8 @@ def request_check(user_input):
         print("Invalid Input!")
 
 
-table = PrettyTable()
-table.add_column("Coffee Types", ["Espresso", "Latte", "Cappuccino"])
-table.add_column("Price", ["1.50$", "2.50$", "3.00$"])
-print(table)
+make_table()
 
-menu = Menu()
-money_machine = MoneyMachine()
-coffee_maker = CoffeeMaker()
 
 while not is_process_over:
     request = input("What would you like? (espresso/latte/cappuccino) ").lower()
