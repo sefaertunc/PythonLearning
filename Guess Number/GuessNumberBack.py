6da01fd428d40731f7b80d2f3a48264aa11469b8
Sefa Ertunç <sefaertnc@gmail.com>
2024-01-19 18:13:53 +0300

    UP: Guess Number OOP

---
+++ Guess Number/GuessNumberBack.py
@@ -0,0 +1,44 @@
+import random
+
+EASY_HEALTH = 10
+HARD_HEALTH = 5
+
+
+class GuessNumber:
+
+    def __init__(self):
+        self.lives = 0
+        self.number = 0
+        self.guessed_number = 0
+        self.isOver = False
+
+    def create_number(self):
+        self.number = random.randint(1, 100)
+
+    def identify_difficulty(self):
+        level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+        if level == "easy":
+            self.lives = EASY_HEALTH
+        else:
+            self.lives = HARD_HEALTH
+
+    def guess_number(self):
+        print(f"You have {self.lives} attempts remaining to guess the number.")
+        self.guessed_number = int(input("Guess a number: "))
+
+    def check_number(self):
+        if self.guessed_number > self.number:
+            print("Too high\nGuess again")
+            self.lives -= 1
+        elif self.guessed_number < self.number:
+            print("Too low\nGuess again")
+            self.lives -= 1
+        else:
+            print("Your guess is right. You win!")
+            self.isOver = True
+
+    def check_lives(self):
+        if self.lives == 0:
+            print(f"The number was {self.number}.")
+            print("You don't have attempt to guess. You lost!")
+            self.isOver = True
--- Guess Number/GuessNumber_main.py
+++ Guess Number/GuessNumber_main.py
@@ -1,52 +1,14 @@
-import random
-
-EASY_HEALTH = 10
-HARD_HEALTH = 5
-
-
-def check_number(guess, lives):
-    if guess > the_number:
-        print("Too high\nGuess again")
-        return lives - 1
-    elif guess < the_number:
-        print("Too low\nGuess again")
-        return lives - 1
-    else:
-        print("Your guess is right. You win!")
-
-
-def identify_health(level):
-    if level < 1:
-        return EASY_HEALTH
-    else:
-        return HARD_HEALTH
-
-
-def guess_number():
-    number = int(input("Guess a number: "))
-    return number
-
+from GuessNumberBack import GuessNumber
 
 print("Welcome to the Number Guessing Game!")
 print("You have to choose a number between 1 and 100.")
 
-difficulties = {"easy": 0, "hard": 1}
 is_game_over = False
+the_game = GuessNumber()
+the_game.create_number()
+the_game.identify_difficulty()
 
-the_number = random.randint(1, 100)
-
-difficulty = difficulties[input(
-    "Choose a difficulty. Type 'easy' or 'hard': ")]
-health = identify_health(difficulty)
-
-guessed_number = 0
-
-while guessed_number != the_number:
-    print(f"You have {health} attemps remaining to guess the number.")
-    guessed_number = guess_number()
-    health = check_number(guessed_number, health)
-
-    if health == 0:
-        print(f"The number was {the_number}.")
-        print("You don't have attemp to guess. You lost!")
-        break
+while not the_game.isOver:
+    the_game.guess_number()
+    the_game.check_number()
+    the_game.check_lives()
