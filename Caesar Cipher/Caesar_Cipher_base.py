35b5ef0fae1a2897cf664da6a489bf116ce8a67a
Sefa Ertunç <sefaertnc@gmail.com>
2024-01-29 21:22:02 +0300

    UP: Caesar Cipher Update

---
+++ Caesar Cipher/Caesar_Cipher_base.py
@@ -0,0 +1,58 @@
+from alphabet import alphabet_list
+
+
+class Caesar_Cipher(object):
+    def __init__(self):
+        self.message = ""
+        self.direction = ""
+        self.shift = 0
+
+    def enter_cryption(self):
+        direction = input("\nEncode or Decode:\n").lower()
+        if direction == "encode" or direction == "decode":
+            self.direction = direction
+            self.enter_message()
+            self.enter_shift()
+            self.caesar_cipher()
+        else:
+            print("Wrong instruction !!")
+
+    def enter_message(self):
+        self.message = input("Type your message:\n").lower()
+
+    def enter_shift(self):
+        self.shift = int(input("Type shift number:\n"))
+
+    def caesar_cipher(self):
+        modified_text = ""
+        for letter in self.message:
+            if letter in alphabet_list:
+                position = alphabet_list.index(letter)
+                new_position = 0
+                if self.direction == "encode":
+                    if (position + self.shift) > len(alphabet_list) - 1:
+                        new_position = (position + (self.shift % len(alphabet_list))) - len(alphabet_list)
+                    else:
+                        new_position = position + self.shift
+                elif self.direction == "decode":
+                    new_position = position - self.shift % len(alphabet_list)
+                modified_text += alphabet_list[new_position]
+            else:
+                modified_text += letter
+        print(f"The {self.direction}d text is '{modified_text}'")
+
+    def check_game(self):
+        program = input("Do you want to try again? Y/N\n").lower()
+        if program == "y":
+            self.clear_data()
+            return True
+        elif program == "n":
+            return False
+        else:
+            return False
+
+    def clear_data(self):
+        self.message = ""
+        self.direction = ""
+        self.shift = 0
+
--- Caesar Cipher/Caesar_Cipher_main.py
+++ Caesar Cipher/Caesar_Cipher_main.py
@@ -1,43 +1,8 @@
-from alphabet import alphabet_list
+from Caesar_Cipher_base import Caesar_Cipher
 
-
-def caesar(text_message, var_direction, var_shift):
-    modified_text = ""
-    for letter in text_message:
-        if letter in alphabet_list:
-            position = alphabet_list.index(letter)
-            new_position = 0
-            if var_direction == "encode":
-                if (position + var_shift) > len(alphabet_list) - 1:
-                    new_position = (position +
-                                    (var_shift % len(alphabet_list))) - len(alphabet_list)
-                else:
-                    new_position = position + var_shift
-            elif var_direction == "decode":
-                new_position = position - var_shift % len(alphabet_list)
-            modified_text += alphabet_list[new_position]
-        else:
-            modified_text += letter
-    print(f"The {var_direction}d text is {modified_text}")
-
-
+the_game = Caesar_Cipher()
 program_check = True
 
 while program_check:
-
-    direction = input("\nEncode or Decode:\n").lower()
-
-    if direction == "encode" or direction == "decode":
-        message = input("Type your message:\n").lower()
-        shift = int(input("Type shift number:\n"))
-        caesar(message, direction, shift)
-    else:
-        print("Wrong instruction !!")
-
-    program = input("Do you want to try again? Y/N\n").lower()
-    if program == "y":
-        program_check = True
-    elif program == "n":
-        program_check = False
-    else:
-        program_check = False
+    the_game.enter_cryption()
+    program_check = the_game.check_game()
