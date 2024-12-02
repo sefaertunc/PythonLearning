5d62f88e11cafd578f274ae20abfa2fc8e93f2e0
Sefa Ertunç <sefaertnc@gmail.com>
2024-12-02 14:52:11 +0100

    UP: Pomodoro

--- Pomodoro/Pomodoro_main.py
+++ Pomodoro/Pomodoro_main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import PhotoImage
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -15,8 +16,18 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+	count_down(1 * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+	count_sec = count % 60
+	count_min = count // 60
+	tomato_canvas.itemconfig(timer_text, text=f'{math.floor(count_min):02d}:{count_sec:02d}')
+	if count > 0:
+		window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -28,19 +39,19 @@ window.config(padx=100, pady=50, bg=YELLOW)
 tomato_canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 tomato_canvas.create_image(100, 112, image=tomato_image)
-tomato_canvas.create_text(100,130, text="00:00", fill="white", font= (FONT_NAME, 35, "bold"))
+timer_text = tomato_canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 tomato_canvas.grid(column=1, row=1)
 
 title_label = tk.Label(text="Timer", font=(FONT_NAME, 45, "bold"), bg=YELLOW, fg=GREEN)
 title_label.grid(column=1, row=0)
 
-start_button = tk.Button(width=5,text="Start", font=FONT_NAME)
+start_button = tk.Button(width=5, text="Start", font=FONT_NAME, bg=YELLOW, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = tk.Button(width=5,text="Reset",font=FONT_NAME)
+reset_button = tk.Button(width=5, text="Reset", font=FONT_NAME, bg=YELLOW)
 reset_button.grid(column=2, row=2)
 
-checkmark_text = tk.Label(text="✔", font=(FONT_NAME,20, "bold"), bg=YELLOW, fg=GREEN)
+checkmark_text = tk.Label(text="✔", font=(FONT_NAME, 20, "bold"), bg=YELLOW, fg=GREEN)
 checkmark_text.grid(column=1, row=3)
 
 window.mainloop()
