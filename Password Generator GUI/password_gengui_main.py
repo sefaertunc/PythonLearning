2e6c1b8cda3536fb0c74012627c907f23e54eb25
Sefa Ertunç <sefaertnc@gmail.com>
2024-12-06 21:10:59 +0100

    UPT: Password Generator GUI Part 2

--- Password Generator GUI/password_gengui_main.py
+++ Password Generator GUI/password_gengui_main.py
@@ -1,12 +1,15 @@
 import tkinter as tk
-import random as rd
+from tkinter import messagebox
 from Utilities import general_supplier
 from Utilities import password_generator as pg
+import saver_mech
 
 supplier = general_supplier.GeneralSupplier()
 pass_gen = pg.PasswordGenerator()
 
 FONT_NAME = "Courier"
+file_path_json = "data.json"
+file_path_txt = "data.txt"
 
 
 def generate_password():
@@ -19,50 +22,58 @@ def add_datas():
 	print("In Method")
 	if website_entry.get() != "" and email_entry.get() != "" and password_entry.get() != "":
 		print("In Condition")
-		with open("data.txt", "a") as data_file:
-			data_file.write("Sample\n")
+		if messagebox.askokcancel(title=website_entry.get(),
+								  message=f"E-mail: {email_entry.get()}\n Password: {password_entry.get()}\n Are you sure?",
+								  icon="warning"):
+			saver_mech.add_account(file_path_json, website_entry.get(), email_entry.get(), password_entry.get())
+			saver_mech.add_data(file_path_txt, website_entry.get(), email_entry.get(), password_entry.get())
 	else:
-		warning_window = tk.Toplevel()
-		warning_window.title("Blank Space Error")
-		warning_label = tk.Label(warning_window, text="Please fill in all the fields.", font=(FONT_NAME, 20, "bold"))
-		warning_window.minsize(width=300, height=100)
-		warning_label.pack()
+		messagebox.showinfo("Error", "Please fill in all the blanks!")
+
+	clean_entries()
+
+
+def clean_entries():
+	website_entry.delete(0, tk.END)
+	password_entry.delete(0, tk.END)
 
 
 # region GUI
 window = tk.Tk()
 window.title('Password Generator GUI')
 window.config(padx=20, pady=20)
-window.minsize(500, 350)
+window.minsize(200, 200)
 
-canvas = tk.Canvas(window, width=500, height=350, highlightthickness=0)
+canvas = tk.Canvas(window, width=200, height=200, highlightthickness=0)
 image = tk.PhotoImage(file='logo.png')
-canvas.create_image(250, 175, image=image)
-canvas.grid(column=1, row=0)
+canvas.create_image(100, 100, image=image)
+canvas.grid(column=1, row=0, columnspan=3)
 
-website_label = tk.Label(window, text='Website:', font=(FONT_NAME, 20))
+website_label = tk.Label(window, text='Website:', font=(FONT_NAME, 15))
 website_label.grid(column=0, row=1)
 
-website_entry = tk.Entry(window, width=50)
-website_entry.grid(column=1, row=1)
+website_entry = tk.Entry(window, width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
-email_label = tk.Label(window, text='Email:', font=(FONT_NAME, 20))
+email_label = tk.Label(window, text='Email:', font=(FONT_NAME, 15))
 email_label.grid(column=0, row=2)
 
-email_entry = tk.Entry(window, width=50)
-email_entry.grid(column=1, row=2)
+email_entry = tk.Entry(window, width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "sefaertnc@gmail.com")
 
-password_label = tk.Label(window, text='Password:', font=(FONT_NAME, 20))
+password_label = tk.Label(window, text='Password:', font=(FONT_NAME, 15))
 password_label.grid(column=0, row=3)
 
-password_entry = tk.Entry(window, width=25)
+password_entry = tk.Entry(window, width=18)
 password_entry.grid(column=1, row=3)
 
 generate_button = tk.Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3)
 
-add_button = tk.Button(text="Add", width=48, command=add_datas)
-add_button.grid(column=1, row=4)
+add_button = tk.Button(text="Add", width=33, command=add_datas)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
 
---
+++ Password Generator GUI/saver_mech.py
@@ -0,0 +1,37 @@
+import json
+
+
+# region JSON
+def load_data(file_path):
+	try:
+		with open(file_path, "r") as file:
+			return json.load(file)
+	except FileNotFoundError:
+		return {"accounts": []}  # Return an empty structure if the file doesn't exist
+
+
+# Function to save JSON data
+def save_data(file_path, data):
+	with open(file_path, "w") as file:
+		json.dump(data, file, indent=4)
+
+
+# Function to add a new account
+def add_account(file_path, platform, email, password):
+	data = load_data(file_path)
+	new_account = {
+		"platform": platform,
+		"email": email,
+		"password": password
+	}
+	data["accounts"].append(new_account)
+	save_data(file_path, data)
+
+
+# endregion
+
+# region TXT
+def add_data(file_path_txt, website, email, password):
+	with open(f"{file_path_txt}", "a") as data_file:
+		data_file.write(f"{website} | {email} | {password}\n")
+# endregion
