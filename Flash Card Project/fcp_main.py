75f3c2e8860c87f0e0f7b9fd6f6953556d5f9b87
Sefa Ertunç <sefaertnc@gmail.com>
2024-12-10 13:00:52 +0100

    UPT: Before Data Saving

--- Flash Card Project/fcp_main.py
+++ Flash Card Project/fcp_main.py
@@ -9,37 +9,29 @@ FONT_NAME = "Ariel"
 supplier = general_supplier.GeneralSupplier()
 
 timer = None
-word_dic = None
+word_card = {}
 
 word_file = pd.read_csv("fcp_data.csv")
 word_dic_list = word_file.to_dict(orient="records")
 
 
 def get_random_word():
-	global word_dic
-	word_dic = random.choice(word_dic_list)
-	flash_card_canvas.itemconfig(language_text, text="German")
-	flash_card_canvas.itemconfig(word_text, text=word_dic["German"])
-	count_down(3)
+	global word_card, flip_timer
+	window.after_cancel(flip_timer)
+	word_card = random.choice(word_dic_list)
+	flash_card_canvas.itemconfig(language_text, text="German", fill="black")
+	flash_card_canvas.itemconfig(word_text, text=word_card["German"], fill="black")
+	flash_card_canvas.itemconfig(card_background, image=front_image)
+	flip_timer = window.after(3000, flip_card)
 
 
-def flip_card(word):
-	print(word)
-	global image
+def flip_card():
 	flash_card_canvas.itemconfig(language_text, text="English", fill="white")
-	flash_card_canvas.itemconfig(word_text, text=word["English"], fill="white")
-	image = tk.PhotoImage(file="images/card_back.png")
-	flash_card_canvas.itemconfig(image, image=image)
+	flash_card_canvas.itemconfig(word_text, text=word_card["English"], fill="white")
+	flash_card_canvas.itemconfig(card_background, image=back_image)
 
+def know_the_word():
 
-def count_down(count):
-	global word_dic
-	if count > 0:
-		global timer
-		print(count)
-		timer = window.after(1000, count_down, count - 1)
-	else:
-		flip_card(word_dic)
 
 
 window = tk.Tk()
@@ -47,9 +39,12 @@ window.title("Flash Card Project")
 window.configure(bg=BACKGROUND_COLOR, padx=50, pady=50)
 window.geometry("900x726")
 
+flip_timer = window.after(3000, flip_card)
+
 flash_card_canvas = tk.Canvas(window, width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0)
-image = tk.PhotoImage(file="images/card_front.png")
-flash_card_canvas.create_image(400, 263, image=image)
+front_image = tk.PhotoImage(file="images/card_front.png")
+back_image = tk.PhotoImage(file="images/card_back.png")
+card_background = flash_card_canvas.create_image(400, 263, image=front_image)
 flash_card_canvas.grid(row=0, column=0, columnspan=2)
 
 language_text = flash_card_canvas.create_text(400, 150, text="", font=(FONT_NAME, 40, "italic"), fill="black")
@@ -65,5 +60,6 @@ wrong_button.grid(row=1, column=0)
 right_button.grid(row=1, column=1)
 
 get_random_word()
+
 
 window.mainloop()
