9bfcfbe0699f55b1919ea6cd863d8a1314c78356
Sefa Ertunç <sefaertnc@gmail.com>
2024-12-19 17:34:16 +0100

    UPT: Private API Key

--- Weather SMS Warning/weather_sms_main.py
+++ Weather SMS Warning/weather_sms_main.py
@@ -1,17 +1,20 @@
 import requests as rq
+import os
+from dotenv import load_dotenv
 
-weather_api_key = 'd956d45ce2d24af85cc6c30ce86f35c8'
+load_dotenv("../Utilities/.env")
+
 main_domain = "https://api.openweathermap.org/data/2.5/forecast"
 weather_params = {
 	"lat": 48.208176,
 	"lon": 16.373819,
-	"appid": weather_api_key,
+	"appid": os.getenv("WEATHER_API_KEY"),
 	"units": "metric",
 	"cnt": 4
 }
 weather = rq.get(main_domain, params=weather_params)
 weather.raise_for_status()
 weather_json = weather.json()
-# print(weather_json["list"][1]["weather"][0]["main"])
+
 for days in weather_json["list"]:
 	print(days["weather"][0]["main"])
