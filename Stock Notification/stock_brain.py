8ec9a8b7dd244f8d550a0becccf9b2ca8c6f0221
Sefa Ertunç <sefaertnc@gmail.com>
2024-12-21 14:38:52 +0100

    UPT: Check Values

--- Stock Notification/stock_brain.py
+++ Stock Notification/stock_brain.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+import math
 
 load_dotenv("../.venv/.env")
 
@@ -27,7 +28,21 @@ stock_news_params = {
 	"limit": 50
 }
 
+
+def check_stock_value():
+	data = values_response.json()["Time Series (Daily)"]
+	dates = list(data.keys())
+	close1 = float(data[dates[0]]["4. close"])
+	close2 = float(data[dates[1]]["4. close"])
+	difference = math.fabs(close1 - close2)
+	condition = close2/3
+	if difference > condition:
+		print("Trouble")
+
+
 news_response = requests.get(API_ENDPOINT, params=stock_news_params)
 print(news_response.url)
 values_response = requests.get(API_ENDPOINT, params=stock_value_params)
 print(values_response.url)
+
+check_stock_value()
