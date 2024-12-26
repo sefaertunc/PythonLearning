9077b4e8199f4b46a4e2f4fbdd79c7258c0bb411
Sefa Ertunç <sefaertnc@gmail.com>
2024-12-26 12:24:27 +0100

    EDT: Entries State

--- Pixela/pixela_base.py
+++ Pixela/pixela_base.py
@@ -38,16 +38,26 @@ class PixelaBase:
         if status == 404:
             data = {"date": date, "quantity": quantity}
             response = self.__send_request("POST", self.GRAPH_ENDPOINT, data=data)
-            self.__handle_response(response,"ADDE")
+            self.__handle_response(response, "ADDE")
         elif status == 200:
             pixel_url = f"{self.GRAPH_ENDPOINT}/{date}"  # Correct pixel URL
             data = {"quantity": quantity}
             response = self.__send_request("PUT", pixel_url, data=data)
-            self.__handle_response(response,"UPDATE")
+            self.__handle_response(response, "UPDATE")
         else:
             messagebox.showinfo(title="Pixela", message="Please click the button again.")
             return
 
+    def __delete_stat(self, date):
+        status = self.__check_stat(date)
+        if status == 200:
+            response = self.__send_request("DELETE", f"{self.GRAPH_ENDPOINT}/{date}")
+            self.__handle_response(response, "DELETE")
+        elif status == 404:
+            tkinter.messagebox.showerror(title=str(status), message="There is no data for this date.")
+        else:
+            messagebox.showinfo(title="Pixela", message="Please click the button again.")
+            return
 
 
     def __send_request(self, method, url, data=None):
@@ -81,30 +91,19 @@ class PixelaBase:
             messagebox.showinfo(title=f"{response.status_code}", message=f"The data is successfully {action.lower()}d.")
             self.__clean_entries()
 
-    def __delete_stat(self,date):
-        status = self.__check_stat(date)
-        if status == 200:
-            response = self.__send_request("DELETE", f"{self.GRAPH_ENDPOINT}/{date}")
-            self.__handle_response(response,"DELETE")
-        elif status == 404:
-            tkinter.messagebox.showerror(title=str(status), message="There is no data for this date.")
-        else:
-            messagebox.showinfo(title="Pixela", message="Please click the button again.")
-            return
-
     def __clean_entries(self):
         self.__pixUI.date_entry.delete(0, tk.END)
         self.__pixUI.duration_entry.delete(0, tk.END)
-        self.__pixUI.date_entry.insert(0,"Date:YYYYMMDD")
-        self.__pixUI.duration_entry.insert(0,"Duration:00.0 min")
+        self.__pixUI.date_entry.insert(0, "Date:YYYYMMDD")
+        self.__pixUI.duration_entry.insert(0, "Duration:00.0 min")
         self.__pixUI.date_entry.focus_set()
-
 
     def __initial_settings(self):
         self.__pixUI.upgrade_button.config(command=lambda: self.__update_stat(date=str(self.__pixUI.date_entry.get()),
                                                                               quantity=str(
                                                                                   self.__pixUI.duration_entry.get())))
         self.__pixUI.delete_button.config(command=lambda: self.__delete_stat(date=str(self.__pixUI.date_entry.get())))
+        self.__pixUI.graph_button.config(command=lambda: self.__show_graph_image())
 
     def get_mainloop(self):
         self.__pixUI.window.mainloop()
