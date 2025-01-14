14dc90aeee7264585ab2f1d6510d750b037d1b83
Sefa Ertunç <sefaertnc@gmail.com>
2025-01-14 21:33:13 +0100

    UPT

--- Selenium/selenium_sample.py
+++ Selenium/selenium_sample.py
@@ -1,3 +1,4 @@
+from pandas.io.stata import excessive_string_length_error
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -8,8 +9,12 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.python.org/")
 
-streak = driver.find_elements(By.CSS_SELECTOR, '.small-widget a')
-for element in streak:
-    print(element.text)
+event_dates = driver.find_elements(By.CSS_SELECTOR, '.event-widget ul time')
+event_names = driver.find_elements(By.CSS_SELECTOR, '.event-widget ul a')
+events_dic = {}
+for num in range(len(event_names)):
+    events_dic[num] = {'time': event_dates[num].get_attribute('datetime').split('T')[0], 'name':event_names[num].text}
 
 driver.quit()
+
+print(events_dic)
