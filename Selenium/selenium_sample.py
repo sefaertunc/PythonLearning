e5197ef9a5ccc302528d33adf568495fad247928
Sefa Ertunç <sefaertnc@gmail.com>
2025-01-15 14:33:25 +0100

    UNN

--- Selenium/selenium_sample.py
+++ Selenium/selenium_sample.py
@@ -1,4 +1,3 @@
-from pandas.io.stata import excessive_string_length_error
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -13,7 +12,10 @@ event_dates = driver.find_elements(By.CSS_SELECTOR, '.event-widget ul time')
 event_names = driver.find_elements(By.CSS_SELECTOR, '.event-widget ul a')
 events_dic = {}
 for num in range(len(event_names)):
-    events_dic[num] = {'time': event_dates[num].get_attribute('datetime').split('T')[0], 'name':event_names[num].text}
+    events_dic[num] = {
+        'time': event_dates[num].get_attribute('datetime').split('T')[0],
+        'name':event_names[num].text
+    }
 
 driver.quit()
 
