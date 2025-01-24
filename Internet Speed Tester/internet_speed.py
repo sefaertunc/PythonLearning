74c539f890443da2a724e63805ee626a08dc2e8a
Sefa Ertunç <sefaertnc@gmail.com>
2025-01-24 20:47:53 +0100

    EDT: Clean libraries

--- Internet Speed Tester/internet_speed.py
+++ Internet Speed Tester/internet_speed.py
@@ -1,12 +1,6 @@
 import time
-from logging import raiseExceptions
-
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 
 
 options = webdriver.ChromeOptions()
